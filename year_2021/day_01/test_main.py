6597c3bc743e67df8f1f4c72276ad1b37f3bbc90
Anaël Beutot <abeutot@oserror.org>
2021-12-01 13:46:39 +0100

    2021 day 1 part 2

--- year_2021/day_01/test_main.py
+++ year_2021/day_01/test_main.py
@@ -5,10 +5,23 @@ def count_increases(measurements):
     )
 
 
+def count_increases_window(measurements):
+    windows = [sum(i) for i in zip(measurements, measurements[1:], measurements[2:])]
+    return count_increases(windows)
+
+
+test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
 def test_count_increases():
-    assert count_increases([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 7
+    assert count_increases(test_data) == 7
+
+
+def test_count_increases_window():
+    assert count_increases_window(test_data) == 5
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        print('answer:', count_increases(list(map(int, f.read().strip().split('\n')))))
+        input_ = list(map(int, f.read().strip().split('\n')))
+        print('part1:', count_increases(input_))
+        print('part2:', count_increases_window(input_))
